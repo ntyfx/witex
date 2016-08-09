f560229c42355552fee1b53fdffef86aea49fd73
朱斌 <bin.zhuxbj@wenba100.com>
2016-08-09 20:41:00 +0800

    fix AssistiveMML
    now no long auto render AssistiveMML

--- example/simple/index.js
+++ example/simple/index.js
@@ -1,5 +1,17 @@
-var testText = '$\\therefore 0 < S\\leqslant \\frac{\\sqrt{2}}{2}$';
+var testText = '`a^2` $a^2$ `\\left ( 1+\\dfrac{1}{2} \\right )` `\\left ( 1+\\dfrac{1}{2} \\right )`';
 
+WiTex.init({
+    mathjax: {
+        tex2jax: {
+            inlineMath: [
+                ['`', '`'],
+                ['$', '$'],
+                ["\\(", "\\)"]
+
+            ]
+        }
+    }
+});
 WiTex.render(testText, function(result) {
     document.getElementById('second').innerHTML = result.html;
 });
